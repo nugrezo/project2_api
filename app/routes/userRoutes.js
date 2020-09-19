65c2966efc760f8e98182f7aac4bf5465f3078f0
Ergun Oz <ergun.oz82@gmail.com>
2020-09-18 23:04:46 -0400

    userRoute sign-up updated

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -83,38 +83,6 @@ router.post('/sign-in', (req, res, next) => {
     .catch(next)
 })
 
-// SIGN UP
-// POST /sign-up
-router.post('/sign-up', (req, res, next) => {
-  // start a promise chain, so that any errors will pass to `handle`
-  Promise.resolve(req.body.credentials)
-    // reject any requests where `credentials.password` is not present, or where
-    // the password is an empty string
-    .then(credentials => {
-      if (!credentials ||
-          !credentials.password ||
-          credentials.password !== credentials.password_confirmation) {
-        throw new BadParamsError()
-      }
-    })
-    // generate a hash from the provided password, returning a promise
-    .then(() => bcrypt.hash(req.body.credentials.password, bcryptSaltRounds))
-    .then(hash => {
-      // return necessary params to create a user
-      return {
-        email: req.body.credentials.email,
-        hashedPassword: hash
-      }
-    })
-    // create user with provided email and hashed password
-    .then(user => User.create(user))
-    // send the new user object back with status 201, but `hashedPassword`
-    // won't be send because of the `transform` in the User model
-    .then(user => res.status(201).json({ user: user.toObject() }))
-    // pass any errors along to the error handler
-    .catch(next)
-})
-
 // CHANGE password
 // PATCH /change-password
 router.patch('/change-password', requireToken, (req, res, next) => {
